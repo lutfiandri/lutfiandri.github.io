e3ed8445ec959aa36218a9250c2776401333b72a
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-12-11 21:00:02 +0700

    feat(navbar): create mobile navbar template

---
+++ components/template/navbar/DesktopNavbar.js
@@ -0,0 +1,58 @@
+import clsx from 'clsx';
+import Container from 'components/element/Container';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useCallback, useMemo } from 'react';
+import ROUTES from './ROUTES';
+
+function DesktopNavbar({ show = false }) {
+  const router = useRouter();
+
+  const { theme, setTheme } = useTheme();
+
+  const isDark = useMemo(() => theme === 'dark', [theme]);
+  const setIsDark = useCallback(
+    (value) => setTheme(value ? 'dark' : 'light'),
+    [setTheme]
+  );
+
+  return (
+    <nav className="sticky top-0 z-50 grid h-[64px] place-items-center bg-gray-light text-gray-dark">
+      <Container>
+        <div className="flex items-center justify-between">
+          <div className="flex items-baseline">
+            <Link href="/">
+              <div role="button" className="py-4 text-lg font-bold text-blue">
+                Lutfi Andriyanto
+              </div>
+            </Link>
+            <div className="w-12"></div>
+            <div className="flex">
+              {ROUTES.map((route) => (
+                <Link key={route.name} href={route.url}>
+                  <div
+                    role="button"
+                    className={clsx(
+                      'm-1 py-4 px-3',
+                      router?.pathname === route.url
+                        ? 'font-bold'
+                        : 'font-semibold'
+                    )}
+                  >
+                    {route.name}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+          {/* <div role="button" onClick={() => setIsDark(!isDark)}>
+            <SunIcon2 isDark={isDark} />
+          </div> */}
+        </div>
+      </Container>
+    </nav>
+  );
+}
+
+export default DesktopNavbar;
--- components/template/navbar/Navbar.js
+++ components/template/navbar/Navbar.js
@@ -1,67 +1,16 @@
-import clsx from 'clsx';
-import Container from 'components/element/Container';
-import { useTheme } from 'next-themes';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useCallback, useMemo } from 'react';
-
-const ROUTES = [
-  {
-    name: 'Article',
-    url: '/article',
-  },
-  {
-    name: 'Portfolio',
-    url: '/portfolio',
-  },
-];
+import DesktopNavbar from './DesktopNavbar';
+import MobileNavbar from './MobileNavbar';
 
 function Navbar() {
-  const router = useRouter();
-
-  const { theme, setTheme } = useTheme();
-
-  const isDark = useMemo(() => theme === 'dark', [theme]);
-  const setIsDark = useCallback(
-    (value) => setTheme(value ? 'dark' : 'light'),
-    [setTheme]
-  );
-
   return (
-    <nav className="sticky top-0 z-50 bg-gray-light text-gray-dark dark:bg-gray-dark dark:text-gray-light">
-      <Container>
-        <div className="flex items-center justify-between">
-          <div className="flex items-baseline">
-            <Link href="/">
-              <div role="button" className="py-4 text-lg font-bold text-blue">
-                Lutfi Andriyanto
-              </div>
-            </Link>
-            <div className="w-12"></div>
-            <div className="flex">
-              {ROUTES.map((route) => (
-                <Link key={route.name} href={route.url}>
-                  <div
-                    role="button"
-                    className={clsx(
-                      'm-1 py-4 px-3',
-                      router?.pathname === route.url
-                        ? 'font-bold'
-                        : 'font-semibold'
-                    )}
-                  >
-                    {route.name}
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
-          {/* <div role="button" onClick={() => setIsDark(!isDark)}>
-            <SunIcon2 isDark={isDark} />
-          </div> */}
-        </div>
-      </Container>
-    </nav>
+    <>
+      <div className="sticky top-0 z-50 block md:hidden">
+        <MobileNavbar />
+      </div>
+      <div className="sticky top-0 z-50 hidden md:block">
+        <DesktopNavbar />
+      </div>
+    </>
   );
 }
 
