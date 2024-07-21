ff5271499fee3a9b0961e2d67aa5fc35972eb974
lutfiandri <lutfiandri232@gmail.com>
2024-07-21 21:53:22 +0700

    feat: add Lutfi Andriyanto to title

--- components/layout/DefaultLayout.js
+++ components/layout/DefaultLayout.js
@@ -1,12 +1,19 @@
 import Footer from 'components/template/footer/Footer';
 import Navbar from 'components/template/navbar/Navbar';
 import Head from 'next/head';
+import { useMemo } from 'react';
 
 function DefaultLayout({ children, title = 'Lutfi Andriyanto' }) {
+  const formattedTitle = useMemo(() => {
+    return title.includes('Lutfi Andriyanto')
+      ? title
+      : `${title} | Lutfi Andriyanto`;
+  }, [title]);
+
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{formattedTitle}</title>
       </Head>
       <Navbar />
       <main>{children}</main>
