3d15ccbe860fb7a628830aabd6326417edb98723
lutfiandri <lutfiandri232@gmail.com>
2025-02-10 15:57:18 +0700

    feat: add sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,30 @@
+const siteUrl = process.env.NEXT_PUBLIC_BASEURL;
+
+// add your private routes here
+const exclude = ['/admin*', '/uploads*', '/images*', '/img*', '/maps*'];
+
+// Save crawling budget by not fetching SSG meta files
+const NEXT_SSG_FILES = [
+  '/*.json$',
+  '/*_buildManifest.js$',
+  '/*_middlewareManifest.js$',
+  '/*_ssgManifest.js$',
+  '/*.js$',
+];
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl,
+  generateRobotsTxt: true,
+  exclude,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
+};
+
+module.exports = config;
